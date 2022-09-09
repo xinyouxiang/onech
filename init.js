45013500ace4c4390885f027e71b09b10730a7de
xinyouxiang <101253589+xinyouxiang@users.noreply.github.com>
2022-09-09 18:36:33 +0800

    Update init.js

--- init.js
+++ init.js
@@ -28,9 +28,7 @@ $(function() {
     }else{
         console.log('未安装钱包插件！');
     }
-————————————————
-版权声明：本文为CSDN博主「m0_67401228」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/m0_67401228/article/details/123377465
+
     // 初始化账户信息
     initAccountSelect();
 });
